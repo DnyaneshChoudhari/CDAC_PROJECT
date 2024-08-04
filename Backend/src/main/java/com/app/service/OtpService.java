1e35b7a70bc9b43d3fb002f600ad76f20559e9bf
shaileshkale244 <shaileshkale2445@gmail.com>
2024-08-04 16:09:30 +0530

    otp service update

--- Backend/src/main/java/com/app/service/OtpService.java
+++ Backend/src/main/java/com/app/service/OtpService.java
@@ -3,15 +3,9 @@ package com.app.service;
 import com.app.entities.Otp;
 
 public interface OtpService {
-<<<<<<< HEAD
 	Otp getOtpById(Long id);
 
 	Otp getOtpByOrderId(Long oid);
-=======
-	Optional<Otp> getOtpById(Long id);
-
-	Optional<Otp> getOtpByOrderId(Long oid);
->>>>>>> 2922e6c5dfbf606ffd3bff99206e4b321894f685
 
 	Otp createOtp(Otp otp);
 }
